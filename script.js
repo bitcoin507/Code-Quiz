b321fd881fe9139d63d3b90071edd4f566196036
Craig <panamaworldwideinvestments@gmail.com>
2022-04-20 00:00:48 +0100

    adding files

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 
+
+
+
+
+
+
 function myFunction(){
 
-document.getElementById('carousel1').setAttribute("class","carousel-item active");
-document.querySelector('button').style.display='none';
-document.getElementById('caption2').style.display='block';
-
-
-
-        
-
-};
-
-
-
-
-
-
-
-
-  
+    document.getElementById('carousel1').setAttribute("class","carousel-item active");
+    document.querySelector('button').style.display='none';
+    document.getElementById('caption2').style.display='block';
+    
+    
+    
+            
+    
+    };
+    
+    
+    
+    
+    
+    
+    
+    
+      
